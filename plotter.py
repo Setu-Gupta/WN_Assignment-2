f31ee808bac0519ab652fefc3bfc1285722e0da8
Setu Gupta <setu.gupta.2020@gmail.com>
2023-04-12 21:06:35 +0530

    Added all the data and plots, and updated plotter
    Updated README as well
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   plots/q1.png
    	new file:   plots/q2a.png
    	new file:   plots/q2b.png
    	new file:   plots/q3.png
    	new file:   plots/q4a.png
    	new file:   plots/q4b.png
    	new file:   plots/q4c.png
    	new file:   plots/q5.png
    	new file:   plots/q5_noloss.png
    	modified:   plotter.py
    	modified:   scripts/README
    	new file:   scripts/q5_noloss.sh
    	new file:   traces/q1
    	new file:   traces/q2a
    	new file:   traces/q2b
    	new file:   traces/q3
    	new file:   traces/q4a
    	new file:   traces/q4b
    	new file:   traces/q4c
    	new file:   traces/q5
    	new file:   traces/q5_noloss

--- plotter.py
+++ plotter.py
@@ -11,12 +11,12 @@ with open(sys.argv[1], 'r') as tracefile:
     for line in tracefile.readlines():
         throughput = float(line.split()[6])
         unit = line.split()[7]
-        if "Kbit" in unit:
+        if "kbit" in unit.lower():
            points.append(throughput/1000)
         points.append(throughput)
 
 # Plot the trace
-plt.title(sys.argv[3])
+plt.title(" ".join(sys.argv[3:]))
 plt.xlabel("Time steps in seconds")
 plt.ylabel("Throughput in Mbit/sec")
 plt.autoscale(enable=True, axis='x', tight=True)
---
+++ scripts/q5_noloss.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+        echo "Usage: q5.sh <interface>"
+        exit -1
+fi
+
+sudo tc qdisc add dev $1 root netem duplicate 30%
+sudo tc qdisc list dev $1
