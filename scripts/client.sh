0ad0a4eb2204ca7b6d00921b4cc1192911dea16b
Setu Gupta <setu.gupta.2020@gmail.com>
2023-04-12 14:37:33 +0530

    Added usage instructions
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   scripts/README
    	modified:   scripts/client.sh

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-        echo "Usage: client.sh <server IP> <port>"
+if [ "$#" -ne 3 ]; then
+        echo "Usage: client.sh <server IP> <port> <trace file path>"
         exit -1
 fi
 echo "Starting the client. Talking to $1:$2"
-iperf3 -c $1 -p $2 -t 120
+iperf3 -c $1 -p $2 -t 120 | grep "/sec" | grep -v "sender" | grep -v "receiver" > $3
